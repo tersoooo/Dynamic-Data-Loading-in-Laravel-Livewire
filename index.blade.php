b872716c72c17eba490056e4f2ebe4aa298d2c58
tersoo <latifwork14@gmail.com>
2024-10-21 21:08:58 +0300

    index.blade.php

--- index.blade.php
+++ index.blade.php
@@ -20,33 +20,33 @@
             <a href="#">
                 <h3 class="text-2xl font-semibold">@username</h3>
             </a>
-            <p class="text-sm">@email</p>
+            <p class="text-sm">terso@terso.com</p>
         </article>
         <article class="text-wrap border border-[#333] p-4">
             <a href="#">
                 <h3 class="text-2xl font-semibold">@username</h3>
             </a>
-            <p class="text-sm">@email</p>
+            <p class="text-sm">terso@terso.com</p>
         </article>
         <article class="text-wrap border border-[#333] p-4">
             <a href="#">
                 <h3 class="text-2xl font-semibold">@username</h3>
             </a>
-            <p class="text-sm">@email</p>
+            <p class="text-sm">terso@terso.com</p>
         </article>
         <article class="text-wrap border border-[#333] p-4">
             <a href="#">
                 <h3 class="text-2xl font-semibold">@username</h3>
             </a>
-            <p class="text-sm">@email</p>
+            <p class="text-sm">terso@terso.com</p>
         </article>
     </div>
-    <div class="bg-[#222] flex justify-center mt-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <div class="bg-[#222] flex justify-center mt-2 border border-[#444] p-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
-    </div>
-</div>
+        <div>
+        </div>
 
 @livewireScripts
 </body>
